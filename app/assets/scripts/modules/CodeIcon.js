49e2730870cf28b4aac0175202dc189eb3ab5dca
Zichun Wang <zichunwang@Zichuns-MacBook-Pro.local>
2022-11-15 01:04:30 -0500

    suprress useless console infos in the module of CodeIcon

--- app/assets/scripts/modules/CodeIcon.js
+++ app/assets/scripts/modules/CodeIcon.js
@@ -6,16 +6,16 @@ axios.get('https://en.wikipedia.org/w/api.php?format=json&action=query&generator
            ).then(res =>{
                let code_icon = document.querySelector(".code-icon")
                
-                console.log(res.data.query.pages)
+                //console.log(res.data.query.pages)
                 let temp_arr = res.data.query.pages
                 let temp_number
                 let temp_title
-                console.log(temp_arr)
+                //console.log(temp_arr)
                 Object.keys(temp_arr).forEach(prop => {
                    temp_number = prop
-                   console.log(temp_number)
+                   //console.log(temp_number)
                    temp_title = temp_arr[temp_number].title
-                   console.log(temp_arr[temp_number].title)
+                   //console.log(temp_arr[temp_number].title)
                    })
                 code_icon.setAttribute("href", `http://en.wikipedia.org/wiki?curid=${temp_number}`)
                 code_icon.setAttribute("title",`Suprise. My code lead you to a random wiki page: ${temp_title}`)
